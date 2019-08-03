0bd02dc3f15634d2154680c8371f17b48d3de944
KienHuynh <hckien@hcmut.edu.vn>
2019-08-03 09:14:44 -0400

    Implement ecld2DDynamicSolve11

--- ndrones/ndrones/base.cpp
+++ ndrones/ndrones/base.cpp
@@ -13,6 +13,24 @@ Point2D::Point2D(float _x, float _y) {
 }
 
 
+float Point2D::l2Distance(Point2D a, Point2D b) {
+	float tmp = (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y);
+	return sqrt(tmp);
+}
+
+
+float Point2D::abs(Point2D a) {
+	return sqrt(a.x*a.x + a.y*a.y);
+}
+
+
+Point2D Point2D::operator-(Point2D const &p) {
+	Point2D tmp;
+	tmp.x = x - p.x;
+	tmp.y = y - p.y;
+}
+
+
 PointState::PointState(Point2D _p) {
 	p = _p;
 	bestTime = -1;
@@ -51,16 +69,34 @@ Agent::Agent(Point2D _loc0, float _v) {
 }
 
 
+float Agent::timing(Point2D p) {
+	float d = Point2D::l2Distance(p, loc);
+	return d / v;
+}
+
+
+float Agent::timing(Point2D i, Point2D j) {
+	float d = Point2D::l2Distance(i, j);
+	return d / v;
+}
+
+
+bool Agent::operator<(Agent const &obj) {
+	return v < obj.v;
+}
+
+
 void Scenario::loadFile(const char* fname) {
 	std::ifstream myfile;
 	myfile.open(fname, std::ios::in);
 
 	int nPackage = 0, nTarget = 0, nAgent = 0;
-
-	myfile >> minX >> maxX >> minY >> maxY;
-	for (int i = minX; i < maxX; i++) {
-		for (int j = minY; j < maxY; j++) {
-			points.push_back(Point2D(i, j));
+	int stepX, stepY;
+	myfile >> minX >> maxX >> stepX >> minY >> maxY >> stepY;
+	// TODO: Make this more efficient
+	for (int i = minX; i < maxX; i += stepX) {
+		for (int j = minY; j < maxY; j += stepY) {
+			points.push_back(PointState(Point2D(i, j)));
 		}
 	}
 
@@ -69,6 +105,19 @@ void Scenario::loadFile(const char* fname) {
 		int x, y;
 		myfile >> x >> y;
 		packages.push_back(Package(Point2D(x, y)));
+		bool in = false;
+		for (int p = 0; p < points.size(); p++) {
+			Point2D tmp = points[p].p;
+			if (tmp.x == packages[packages.size() - 1].loc0.x &&
+				tmp.y == packages[packages.size() - 1].loc0.y) {
+				packageIdx.push_back(p);
+				in = true;
+			}
+		}
+		if (in == false) {
+			points.push_back(PointState(packages[packages.size()-1].loc0));
+			packageIdx.push_back(points.size() - 1);
+		}
 	}
 
 	myfile >> nTarget;
@@ -76,6 +125,20 @@ void Scenario::loadFile(const char* fname) {
 		int x, y;
 		myfile >> x >> y;
 		targets.push_back(Target(Point2D(x, y)));
+
+		bool in = false;
+		for (int p = 0; p < points.size(); p++) {
+			Point2D tmp = points[p].p;
+			if (tmp.x == targets[targets.size() - 1].loc.x &&
+				tmp.y == targets[targets.size() - 1].loc.y) {
+				targetIdx.push_back(p);
+				in = true;
+			}
+		}
+		if (in == false) {
+			points.push_back(PointState(targets[targets.size() - 1].loc));
+			targetIdx.push_back(points.size() - 1);
+		}
 	}
 
 	myfile >> nAgent;
@@ -100,11 +163,63 @@ void Scenario::loadFile(const char* fname) {
 		Agent a(x, y, v);
 		agents.push_back(a);
 	}
+	// Sort the agents by speed
+	std::sort(agents.begin(), agents.end());
 }
 
 
-void Scenario::ecld2DDynamicSolve() {
+float max(float x, float y) {
+	if (x > y) return x;
+	else return y;
+}
 
+
+void Scenario::ecld2DDynamicSolve11() {
+	// Find the drone that can get to the package in the shortest time
+	float bestTime = agents[0].timing(packages[0].loc0);
+	int bestAgent = 0;
+	for (int k = 1; k < agents.size(); k++) {
+		float timeTmp = agents[k].timing(packages[0].loc0);
+		if (timeTmp < bestTime) {
+			bestTime = timeTmp;
+			bestAgent = k;
+		}
+	}
+
+	// Assign best time, agent queue and point queue to all points in the scenario
+	for (int i = 0; i < points.size(); i++) {
+		points[i].bestTime = agents[bestAgent].timing(points[i].p);
+		points[i].agentQueue.push_back(agents[bestAgent]);
+		points[i].pointQueue.push_back(packages[0].loc0);
+	}
+
+	// No point in handing the package to a slower drone
+	for (int k = bestAgent+1; k < agents.size(); k++) {
+		std::vector<PointState> pointsCopy = points;
+		for (int i = 0; i < points.size(); i++) {
+			// Find the best handoff point j so that it can travel to i faster
+			int best_j = -1;
+			if (i % 10 == 0) std::cout << i << std::endl;
+			for (int j = 0; j < pointsCopy.size(); j++) {
+				float timeTo_j = agents[k].timing(pointsCopy[j].p);
+				// Factor waiting time
+				timeTo_j = max(timeTo_j, pointsCopy[j].bestTime);
+
+				float time_jTo_i = agents[k].timing(pointsCopy[j].p, pointsCopy[i].p);
+				if (timeTo_j + time_jTo_i < pointsCopy[i].bestTime) {
+					
+					points[i].bestTime = timeTo_j + time_jTo_i;
+					best_j = j;
+				}
+			}
+			if (best_j > -1) {
+				points[i].agentQueue = pointsCopy[best_j].agentQueue;
+				points[i].agentQueue.push_back(agents[k]);
+				points[i].pointQueue = pointsCopy[best_j].pointQueue;
+				points[i].pointQueue.push_back(pointsCopy[best_j].p);
+			}
+		}
+	}
 }
 
 
@@ -112,7 +227,9 @@ void Scenario::solve(Solver solver) {
 	if (targets.size() == 1) {
 		if (packages.size() == 1) {
 			if (solver == Solver::ECLD_2D_DYNAMIC) {
-				ecld2DDynamicSolve();
+				ecld2DDynamicSolve11();
+				int a = 1;
+				a = a + 1;
 			}
 		}
 	}
--- ndrones/ndrones/base.h
+++ ndrones/ndrones/base.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 #include <fstream>
+#include <cmath>
+#include <iostream>
 
 enum Solver {ECLD_2D_DYNAMIC};
 
@@ -15,6 +18,11 @@ public:
 	Point2D(float, float);
 
 	void copy(Point2D);
+	// Compute L2 distance
+	static float l2Distance(Point2D, Point2D);
+	static float abs(Point2D);
+
+	Point2D operator - (Point2D const &obj);
 };
 
 
@@ -22,6 +30,14 @@ class PointState {
 public:
 	Point2D p;
 	std::vector<Agent> agentQueue;
+	// Queue of points that were previously visited by the above agents
+	// Example:
+	// Agent a1->a2
+	// PQueue p1->p2
+	// This means that a1 visited the package at p1
+	// a1 then moved to p2 to meet a2
+	// a2 then moved to *this* point to meet some other drones
+	std::vector<Point2D> pointQueue;
 	float bestTime;
 
 	PointState(Point2D _p);
@@ -45,6 +61,14 @@ public:
 
 	Agent(int _x, int _y, float _v);
 	Agent(Point2D _loc0, float _v);
+
+	// Compute the time to go to point p based on current loc and velocity
+	float timing(Point2D);
+	// Compute the time to go from point i to j
+	float timing(Point2D, Point2D);
+
+
+	bool operator < (Agent const &obj);
 };
 
 
@@ -70,13 +94,19 @@ public:
 	std::vector<Agent> agents;
 	float maxSpeed, minSpeed;
 	int minX, maxX, minY, maxY;
-	std::vector<Point2D> points;
-	std::vector<Package> packages;
-	std::vector<Target> targets;
 
+	std::vector<PointState> points;
+	std::vector<Package> packages;
+	// Store the id of the point of the package
+	std::vector<int> packageIdx;
+	std::vector<Target> targets;
+	std::vector<int> targetIdx;
+
+	// Load scenario from file
 	void loadFile(const char*);
 
-	void ecld2DDynamicSolve();
+	// Euclidean 2D dynamic solver for 1-drone-1-package
+	void ecld2DDynamicSolve11();
 	void solve(Solver);
 };
 
