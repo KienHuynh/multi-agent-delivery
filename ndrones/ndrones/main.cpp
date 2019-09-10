005227bb63fda50bdc0e3cb79caa50325abb4570
KienHuynh <hckien@hcmut.edu.vn>
2019-09-10 09:34:57 -0400

    Use cxxopts to add cmd args to main function

--- ndrones/ndrones/main.cpp
+++ ndrones/ndrones/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
-
+#include <cxxopts.hpp>
 
 // GUI
 #include "gui.h"
 
-
 int main(int argc, char **argv) {
-	GUI gui(1080, 720);
-	return(Fl::run());
+	cxxopts::Options options("MyProgram", "One line description of MyProgram");
+	options.add_options()
+		("g, gui", "Turning GUI on or not", cxxopts::value<int>())
+		("i, input", "Path to input file", cxxopts::value<std::string>())
+		("o, output", "Path to output file", cxxopts::value<std::string>())
+		;
+	auto cmd = options.parse(argc, argv);
+	int guiOn = cmd["g"].as<int>();
+	
+	if (guiOn) {
+		GUI gui(1080, 720);
+		return(Fl::run());
+	}
+	else {
+		std::string inputFile = cmd["i"].as<std::string>();
+		std::string outputFile = cmd["o"].as<std::string>();
+
+		Scenario scenario;
+		scenario.loadFile(inputFile.c_str());
+		scenario.solve();
+		scenario.writeSolution(outputFile.c_str());
+
+	}
+	
 }
 	
