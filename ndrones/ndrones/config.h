c9ca05d52b74ada2a60e4807b50d2a89b3817cd3
KienHuynh <hckien@hcmut.edu.vn>
2019-09-10 09:32:16 -0400

    Add stepX and stepY to config.h

--- ndrones/ndrones/config.h
+++ ndrones/ndrones/config.h
@@ -1,5 +1,12 @@
 #pragma once
 namespace cfg {
 	const float timeStep = 0.1;
-	const int polySamplingRate = 5;
+
+	// Step size for sampling the points on polygon edges
+	const int polySamplingRate = 3;
+
+	// Step size for sampling the grid points
+	// If these two are > 0, they will override the values in the input file
+	const float stepX = -1;
+	const float stepY = -1;
 }
