a08f2a0d45accd6cc8ed4509775b30d31815eb76
KienHuynh <hckien@hcmut.edu.vn>
2019-11-08 12:00:17 -0500

    Add config values for circular grid generation

--- ndrones/ndrones/config.h
+++ ndrones/ndrones/config.h
@@ -7,8 +7,12 @@ namespace cfg {
 
 	// Step size for sampling the grid points
 	// If these two are > 0, they will override the values in the input file
+	// For debug purpose only
 	const float stepX = -1;
 	const float stepY = -1;
+	// Circular grid parameters
+	const float theta = -1;
+	const float nR = -1;
 
 	// Number of colors for the palette
 	const int numColor = 2048;
